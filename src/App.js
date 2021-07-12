1c8ae03f2274f44d902d26a9eca4733f9872956b
codubee <info@codubee.com>
2021-07-11 22:29:32 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import LandingPage from './views/LandingPage';
+
+function App() {
+  return (
+      <LandingPage />
+  );
+}
+
+export default App;
---
+++ src/views/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function LandingPage() 
+{
+  return (
+        <div>
+            <h1>Hello world</h1>
+        </div>
+    )
+}
+
+export default LandingPage;
