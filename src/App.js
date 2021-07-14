f73e05bbd4cf5897b0da04fd452b308a4e98a98b
Brandon Lopez <brandon.lopez1214@gmail.com>
2021-07-14 10:25:31 -0500

    Simple Navbar implementation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Navigation from './components/Navigation';
 import LandingPage from './views/LandingPage';
+
 
 function App() {
   return (
+    <div>
+      <Navigation />
       <LandingPage />
+    </div>
+      
   );
 }
 
