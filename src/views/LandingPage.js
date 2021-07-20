a42edd62f0b7f55bd4c529de0ff340115599672f
abhikTheFirst <35345368+abhikTheFirst@users.noreply.github.com>
2021-07-20 11:35:42 -0500

    First Commit, adding the Features Component

--- src/views/LandingPage.js
+++ src/views/LandingPage.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Features from '../components/Features';
 
 function LandingPage() 
 {
   return (
         <div>
-            <h1>Hello world</h1>
+            <Features></Features>
         </div>
     )
 }
