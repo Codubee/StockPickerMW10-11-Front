da90c21e3e92784bca4b7f059ce5c2cf09bc4a63
Sakamoto448 <joelaronpatino@gmail.com>
2021-07-21 10:05:07 -0500

    added Tools component

--- src/components/Tools.js
+++ src/components/Tools.js
@@ -1,18 +1,46 @@
 import React from 'react';
+import './Tools.css';
 
 function Tools() 
 {
   return (
-      <div>
-        <h1> Jira </h1> 
-
-        <h1> Nodejs </h1> 
-
-        <h1>React</h1> 
-
-        <h1> Postman</h1> 
+    <div>
+    <h3> Tools used for this Project</h3>
+      <ul>
         
-        </div>
+        <li class="pic-list">
+          <img src="https://pluralsight2.imgix.net/paths/images/nodejs-45adbe594d.png"
+          alt="icon" width="50" height="auto" />
+          <p>NodeJs</p>
+        </li>
+
+        <br></br>
+        
+        <li class="pic-list">
+          <img src="https://miro.medium.com/max/512/1*fVBL9mtLJmHIH6YpU7WvHQ.png" 
+          alt="icon" width="50" height="auto" />
+          <p>Postman</p>
+        </li>
+
+        <br></br>
+
+        <li class="pic-list">
+          <img src="https://cdn.icon-icons.com/icons2/2699/PNG/512/atlassian_jira_logo_icon_170511.png" 
+          alt="icon" width="50" height="auto" />
+          <p>Jira</p>
+        </li>
+
+        <br></br>
+
+        <li class="pic-list">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png" 
+          alt="icon" width="50" height="auto" />
+          <p>React</p>
+        </li>
+
+      </ul>
+
+    </div>
     );
 }
 
