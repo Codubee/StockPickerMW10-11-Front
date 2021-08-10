1e12a75701dbde817c40e50c987f1e2bf1e7349d
Joel Aron Patino-Guzman <65023392+Sakamoto448@users.noreply.github.com>
2021-08-09 23:04:59 -0500

    Update LandingPage.js

--- src/views/LandingPage.js
+++ src/views/LandingPage.js
@@ -2,11 +2,10 @@ import React from 'react';
 import Features from '../components/Features';
 import Tools from '../components/Tools';
 import ExplanationPage from '../components/Explanation';
-<<<<<<< HEAD
 import ButtonClick from '../components/ButtonClick';
-=======
 
->>>>>>> aec5a67f89831d12f5844b3380728d88674a8338
+
+
 
 function LandingPage() 
 {
@@ -14,11 +13,7 @@ function LandingPage()
         <div>
             <ExplanationPage/>
             <Features></Features>
-<<<<<<< HEAD
             <ButtonClick/>
-
-=======
->>>>>>> aec5a67f89831d12f5844b3380728d88674a8338
         </div>
     )
 }
