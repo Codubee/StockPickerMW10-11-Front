b7d8a7138d4009d963d67671cd428cc9937aaf9d
codubee <info@codubee.com>
2021-08-09 22:48:59 -0500

    Added view to the match page

--- src/views/Match.js
+++ src/views/Match.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Buttons from '../components/Buttons'
+import View from '../components/View'
 
 class Match extends React.Component {
     render() {
         return (
             <div>
                 <Buttons />
+                <View/>
             </div>
         )
     }
