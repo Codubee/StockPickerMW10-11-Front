fb54ede0fe9beac14542127f305febbea6f29744
codubee <info@codubee.com>
2021-08-09 22:46:49 -0500

    Added the view component

--- src/views/Match.js
+++ src/views/Match.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Buttons from '../components/Buttons'
 
-class Match extends React.Component{
-    render(){
-        return(
-                <div>
-                    <Buttons/>
-                </div>
+class Match extends React.Component {
+    render() {
+        return (
+            <div>
+                <Buttons />
+            </div>
         )
     }
 }
