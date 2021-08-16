fcf9fe13150cbd2b8e89a828ddecd3bf750af986
codubee <info@codubee.com>
2021-08-16 10:33:07 -0500

    Added description to match page

--- src/components/Description.js
+++ src/components/Description.js
@@ -3,11 +3,16 @@ import React from 'react'
 
 function Description(props){
 
-    return(
-        <div>
-            <p>{props.companyName}, {props.symbol}, {props.price}</p>
-        </div>
-    )
+    if(props.price){
+        return(
+            <div>
+                <p>{props.companyName}, {props.symbol}, {props.price}</p>
+            </div>
+        )
+    }
+    else{
+        return <></>
+    }
 }
 
 export default Description
