1a2280d2931f0876325d13e3afc54b2724e94d65
codubee <info@codubee.com>
2021-08-16 10:15:04 -0500

    moved buttons to match page

--- src/views/Match.js
+++ src/views/Match.js
@@ -1,12 +1,30 @@
 import React from 'react'
-import Buttons from '../components/Buttons'
-
+import {Button} from 'reactstrap'
 class Match extends React.Component{
+    constructor() {
+        super()
+        //Setting a default value
+        this.state = { answer: "" }
+     
+        //Bind Methods
+        this.Yes= this.Yes.bind(this)
+        this.No= this.No.bind(this)
+    }
+
+    Yes() {
+        alert('clicked yes')
+    }
+    No() {
+        alert('clicked no')
+    }
+
+
     render(){
         return(
-                <div>
-                    <Buttons/>
-                </div>
+            <div className="text-center">
+                <Button color="danger" size= "lg" onClick={this.No}>No</Button>
+                <Button color="success" size= "lg" onClick={this.Yes}>Yes</Button>
+            </div>
         )
     }
 }
