9b11688908438a316fe27896036633bbf65dad00
abhikTheFirst <35345368+abhikTheFirst@users.noreply.github.com>
2021-08-18 10:46:33 -0500

    Added Delete API

--- src/components/Matches.js
+++ src/components/Matches.js
@@ -1,7 +1,21 @@
 import React from 'react';
-
-function Matches(props){
-
+import axios from 'axios';
+import { render } from '@testing-library/react';
+import { tsConstructorType } from '@babel/types';
+class Matches extends React.Component{
+  constructor(props) {
+    super(props);
+    this.apiTrigger = this.apiTrigger.bind(this);
+  }
+  apiTrigger() {
+    axios.get("https://stockpicker-mw11-12-back.herokuapp.com/deleteStock?userId=99&stockId=1")
+      
+    .then((response) => {
+          console.log(response.data); //Empty response, fill this with the ClickMe info!
+    })
+    
+  } 
+render() {
   const stocks = [
     { company: "Apple", symbol: "appl", price: 12.98 },
     { company: "Microsoft", symbol: "msft", price: 15.98 },
@@ -11,16 +25,16 @@ function Matches(props){
           <div>
               <h1>Current Matches</h1>
               {
-                  
                       stocks.map((matched, id) => (
                       <div key={id}>
                           <p>{matched.company}, {matched.symbol},{matched.price}</p>
                       </div>
                   ))
               }
+              <button onClick={this.apiTrigger}> Delete </button>
           </div>
-
-  );
+        );
+   }
 }
 
 export default Matches;
