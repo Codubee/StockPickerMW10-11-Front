aa5f28987c1fefaeaead1974105702859f23da01
Miguel <85082129+Miguel-S117@users.noreply.github.com>
2021-08-18 10:34:08 -0500

    Added addStock api (#18)

--- src/views/Match.js
+++ src/views/Match.js
@@ -5,25 +5,40 @@ import {Container} from 'reactstrap'
 import Collapsable from '../components/Collapsable'
 import '../styles/Match.css'
 import Description from '../components/Description'
+import axios from 'axios'
 
 class Match extends React.Component{
-    constructor() {
-        super()
+    constructor(props) {
+        super(props)
         //Setting a default value
         this.state = { answer: "" }
      
         //Bind Methods
         this.Yes= this.Yes.bind(this)
         this.No= this.No.bind(this)
+        this.addStock = this.addStock.bind(this);
     }
 
     Yes() {
         alert('clicked yes')
+        this.addStock()
     }
     No() {
         alert('clicked no')
     }
 
+    addStock() {
+        var body = {
+            "userId":"20",
+            "stock":{"companyName":"Microsoft","symbol":"MSFT","image":"","stockId":"19"}
+        }
+
+        axios.post("https://stockpicker-mw11-12-back.herokuapp.com/addStock", body)
+            .then((response) => {
+                console.log(response.data);
+                
+            })
+    }
 
     render(){
         return(
