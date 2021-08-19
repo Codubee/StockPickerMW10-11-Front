957a202134d7592322f2ea2e6c729eea2d837380
Brandon Lopez <brandon.lopez1214@gmail.com>
2021-08-18 22:42:54 -0500

    Final Push review changes

--- src/components/Collapsable.js
+++ src/components/Collapsable.js
@@ -14,21 +14,21 @@ class Collapsable extends React.Component {
 
     toggle(){
         this.setState({isOpen:!this.state.isOpen});
-        
-        triggerGetApi(){
+    }
 
-            axios.get("https://stockpicker-mw11-12-back.herokuapp.com/getMatches?userId=99")
-    
-                .then((response) => {
-    
-                    console.log(response.data);
-    
-                    this.setState({
-                        
-                        stocks: response.data
-                    })
+    triggerGetApi(){
+
+        axios.get("https://stockpicker-mw11-12-back.herokuapp.com/getMatches?userId=99")
+
+            .then((response) => {
+
+                console.log(response.data);
+
+                this.setState({
+                    
+                    stocks: response.data
                 })
-        }
+            })
     }
 
     render(){
