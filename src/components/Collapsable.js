ec2cb495582af9540d09079eb542f76153906071
Brandon Lopez <brandon.lopez1214@gmail.com>
2021-08-18 22:38:59 -0500

    Review error: Miguel

--- src/components/Collapsable.js
+++ src/components/Collapsable.js
@@ -1,17 +1,34 @@
 import React, {useState} from 'react';
 import { Collapse, Button } from 'reactstrap'; //Add whatever elements you need 
-import Matches from '../components/Matches'
+import Matches from '../components/Matches';
+import axios from 'axios';
+
 
 class Collapsable extends React.Component {
 
     constructor(props){
-        super(props)
-        this.state = {isOpen:false}
-        this.toggle = this.toggle.bind(this)
+        super(props);
+        this.state = {isOpen:false};
+        this.toggle = this.toggle.bind(this);
     }
 
     toggle(){
-        this.setState({isOpen:!this.state.isOpen})
+        this.setState({isOpen:!this.state.isOpen});
+        
+        triggerGetApi(){
+
+            axios.get("https://stockpicker-mw11-12-back.herokuapp.com/getMatches?userId=99")
+    
+                .then((response) => {
+    
+                    console.log(response.data);
+    
+                    this.setState({
+                        
+                        stocks: response.data
+                    })
+                })
+        }
     }
 
     render(){
